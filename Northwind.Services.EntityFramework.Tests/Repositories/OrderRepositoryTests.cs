5adf0900db0265e6ae049a8ef8e4d801c610845f
Serhii Yasenev <serhii.yasenev@gmail.com>
2025-04-20 17:09:44 +0300

    update packages

--- Northwind.Services.EntityFramework.Tests/Repositories/OrderRepositoryTests.cs
+++ Northwind.Services.EntityFramework.Tests/Repositories/OrderRepositoryTests.cs
@@ -44,14 +44,14 @@ public sealed class OrderRepositoryTests : RepositoryTestsBase
     [Test]
     public void HasRequiredMembers()
     {
-        Assert.AreEqual(0, this.ClassType.GetFields(BindingFlags.Instance | BindingFlags.Public).Length);
-        Assert.AreEqual(0, this.ClassType.GetConstructors(BindingFlags.Static | BindingFlags.Public).Length);
-        Assert.AreEqual(1, this.ClassType.GetConstructors(BindingFlags.Instance | BindingFlags.Public).Length);
-        Assert.AreEqual(0, this.ClassType.GetProperties(BindingFlags.Static | BindingFlags.Public).Length);
-        Assert.AreEqual(0, this.ClassType.GetProperties(BindingFlags.Instance | BindingFlags.Public).Length);
-        Assert.AreEqual(0, this.ClassType.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.DeclaredOnly).Length);
-        Assert.AreEqual(5, this.ClassType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly).Length);
-        Assert.AreEqual(0, this.ClassType.GetEvents(BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public).Length);
+        Assert.That(this.ClassType.GetFields(BindingFlags.Instance | BindingFlags.Public).Length, Is.EqualTo(0));
+        Assert.That(this.ClassType.GetConstructors(BindingFlags.Static | BindingFlags.Public).Length, Is.EqualTo(0));
+        Assert.That(this.ClassType.GetConstructors(BindingFlags.Instance | BindingFlags.Public).Length, Is.EqualTo(1));
+        Assert.That(this.ClassType.GetProperties(BindingFlags.Static | BindingFlags.Public).Length, Is.EqualTo(0));
+        Assert.That(this.ClassType.GetProperties(BindingFlags.Instance | BindingFlags.Public).Length, Is.EqualTo(0));
+        Assert.That(this.ClassType.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.DeclaredOnly).Length, Is.EqualTo(0));
+        Assert.That(this.ClassType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly).Length, Is.EqualTo(5));
+        Assert.That(this.ClassType.GetEvents(BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public).Length, Is.EqualTo(0));
     }
 
     [TestCase("GetOrdersAsync", false, true, true, typeof(Task<IList<RepositoryOrder>>))]
